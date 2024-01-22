c501a73c74ca0b3e03ec4b78cc4c95bfe3f5a9b5
armoredchimp <mattkasmir@gmail.com>
2024-01-22 12:47:06 -0800

    1/22

--- space-finder/src/services/hello.js
+++ space-finder/src/services/hello.js
@@ -1,6 +1,6 @@
 exports.main = async function (event, context) {
   return {
     statusCode: 200,
-    body: JSON.stringify(`Hello it's-a-me, ${process.env.TABLE_NAME}`),
+    body: JSON.stringify(`Hello from ${process.env.TABLE_NAME}`),
   };
 };
