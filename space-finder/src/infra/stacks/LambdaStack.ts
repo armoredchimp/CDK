491ee8b62c4cd96b8bd89d9fc98e8855582ae77a
armoredchimp <mattkasmir@gmail.com>
2024-01-22 12:40:16 -0800

    1/22

--- space-finder/src/infra/stacks/LambdaStack.ts
+++ space-finder/src/infra/stacks/LambdaStack.ts
@@ -2,16 +2,30 @@ import { Stack, StackProps } from 'aws-cdk-lib'
 import { Construct } from 'constructs'
 import { Code, Function as Lambda, Runtime } from 'aws-cdk-lib/aws-lambda'
 import { join } from 'path'
+import { LambdaIntegration } from 'aws-cdk-lib/aws-apigateway'
+import { ITable } from 'aws-cdk-lib/aws-dynamodb'
+
+interface LambdaStackProps extends StackProps {
+  spacesTable: ITable
+}
 
 
 export class LambdaStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+
+  public readonly helloLambdaIntegration: LambdaIntegration
+
+  constructor(scope: Construct, id: string, props: LambdaStackProps) {
     super(scope, id, props)
 
-    new Lambda(this, 'HelloLambda', {
+    const helloLambda = new Lambda(this, 'HelloLambda', {
       runtime: Runtime.NODEJS_18_X,
       handler: 'hello.main',
-      code: Code.fromAsset(join(__dirname, '..', '..', 'services'))
+      code: Code.fromAsset(join(__dirname, '..', '..', 'services')),
+      environment: {
+        TABLE_NAME: props.spacesTable.tableName
+      }
     })
+
+    this.helloLambdaIntegration = new LambdaIntegration(helloLambda)
   }
 }
--- space-finder/src/services/hello.js
+++ space-finder/src/services/hello.js
@@ -1,6 +1,6 @@
 exports.main = async function (event, context) {
   return {
     statusCode: 200,
-    body: JSON.stringify('Hello from lambda'),
+    body: JSON.stringify(`Hello it's-a-me, ${process.env.TABLE_NAME}`),
   };
 };
