33091a92a3ef005173d0003714534a287156024b
armoredchimp <mattkasmir@gmail.com>
2024-01-29 21:37:24 -0800

    1/29

--- space-finder/src/services/spaces/handler.ts
+++ space-finder/src/services/spaces/handler.ts
@@ -1,21 +1,35 @@
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import { APIGatewayProxyResult, APIGatewayProxyEvent, Context } from 'aws-lambda';
 import { v4 } from 'uuid'
+import { postSpaces } from './PostSpaces';
 
+
+const ddbClient = new DynamoDBClient({});
 
 
 async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
 
   let message: string;
 
-  switch (event.httpMethod) {
-    case 'GET':
-      message = 'Hello from GET!'
-      break;
-    case 'POST':
-      message = 'Hello from POST!'
-      break;
-    default:
-      break;
+
+  try {
+    switch (event.httpMethod) {
+      case 'GET':
+        message = 'Hello from GET!'
+        break;
+      case 'POST':
+        const response = postSpaces(event, ddbClient, context)
+        return response;
+      default:
+        break;
+    }
+  }
+  catch (error) {
+    console.log(error)
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error.message)
+    }
   }
 
 
